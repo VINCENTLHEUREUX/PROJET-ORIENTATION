fc78441c45dd530a9094924bdc49d96dd3817a8b
aminebuu <mohamed-amine.boudi.1@ens.etsmtl.ca>
2025-03-25 12:44:58 -0400

    Update api.js

--- orienter-ets-projet/orienter-ets-projet/01 - frontend/src/services/api.js
+++ orienter-ets-projet/orienter-ets-projet/01 - frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Remplacez par l'URL de votre backend Spring
+  baseURL: 'http://localhost:3306', // Remplacez par l'URL de votre backend Spring 
   headers: {
     'Content-Type': 'application/json',
   },
