51be74dbb9d22f1a1780455e5f4fede44eb10640
aminebuu <mohamed-amine.boudi.1@ens.etsmtl.ca>
2025-03-25 20:35:29 -0400

    Update App.jsx

--- orienter-ets-projet/orienter-ets-projet/01 - frontend/src/01 - frontend2/src/App.jsx
+++ orienter-ets-projet/orienter-ets-projet/01 - frontend/src/01 - frontend2/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Accueil from "./pages/Accueil";
 import Connexion from "./pages/Connexion";
 import Inscription from "./pages/Inscription";
-
+import Orientation from "./pages/Orientation";
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Accueil />} />
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/inscription" element={<Inscription />} />
+        <Route path="/orientation" element = {<Orientation/>} />
       </Routes>
     </Router>
   );
