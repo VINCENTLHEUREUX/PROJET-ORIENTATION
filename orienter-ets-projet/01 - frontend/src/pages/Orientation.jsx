583dfd458d9343d86ea7f515023610ff572505fa
aminebuu <mohamed-amine.boudi.1@ens.etsmtl.ca>
2025-03-31 17:52:20 -0400

    Update Orientation.jsx

--- orienter-ets-projet/01 - frontend/src/pages/Orientation.jsx
+++ orienter-ets-projet/01 - frontend/src/pages/Orientation.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import "../styles/orientation.css";
 import axios from 'axios';
 
