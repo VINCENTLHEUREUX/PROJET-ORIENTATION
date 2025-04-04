817c5cb61397cb22925357db2db9f39be2b0d2ec
VINCENTLHEUREUX <vincent.lheureux.1@ens.etsmtl.ca>
2025-04-04 10:41:14 -0400

    Update Formations.jsx

--- orienter-ets-projet/01 - frontend/src/pages/Formations.jsx
+++ orienter-ets-projet/01 - frontend/src/pages/Formations.jsx
@@ -120,30 +120,7 @@ export default function Formations() {
   return (
     <div className="page-formations">
       {/* Header (identique à Orientation.js) */}
-      <header className="header">
-        <div className="header-container">
-          <Link to="/">
-            <img
-              src="src/images/ETS-rouge-impr-fond_transparent.png"
-              alt="Logo ÉTS"
-              className="logo-ets"
-            />
-          </Link>
-          <div className="header-links">
-            <Link to="/connexion" className="link-ets">Connexion</Link>
-            <Link to="/inscription" className="link-ets">Inscription</Link>
-          </div>
-        </div>
-        <nav className="nav-bar">
-          <ul className="nav-list">
-            <li><Link to="/" className="nav-item">Accueil</Link></li>
-            <li><a href="#" className="nav-item">Premier cycle</a></li>
-            <li><a href="#" className="nav-item">Cycles supérieurs</a></li>
-            <li><a href="#" className="nav-item">Certificats</a></li>
-            <li><a href="#" className="nav-item">Formation continue</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
 
       {/* Contenu principal */}
       <main className="main-content">
@@ -247,41 +224,8 @@ export default function Formations() {
         </div>
       </main>
 
-      {/* Footer (identique à Orientation.js) */}
-      <footer className="footer">
-        <div className="footer-content">
-          <div className="footer-col">
-            <h3>École de technologie supérieure</h3>
-            <p>1100, rue Notre-Dame Ouest</p>
-            <p>Montréal (Québec) H3C 1K3</p>
-            <p>Canada</p>
-            <p>Téléphone: 514 396-8800</p>
-          </div>
-          <div className="footer-col">
-            <h3>Liens rapides</h3>
-            <ul className="footer-links">
-              <li><a href="#">Admission</a></li>
-              <li><a href="#">Programmes</a></li>
-              <li><a href="#">Bibliothèque</a></li>
-              <li><a href="#">Recherche</a></li>
-              <li><a href="#">Plan du site</a></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h3>Restez connectés</h3>
-            <div className="social-links">
-              <a href="#">Facebook</a>
-              <a href="#">Twitter</a>
-              <a href="#">Instagram</a>
-              <a href="#">LinkedIn</a>
-            </div>
-            <p>Abonnez-vous à notre infolettre pour rester informé des dernières nouvelles et événements.</p>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <p>&copy; 2025 École de technologie supérieure. Tous droits réservés.</p>
-        </div>
-      </footer>
+      {/* Footer*/ }
+        <Footer />
     </div>
   );
 }
