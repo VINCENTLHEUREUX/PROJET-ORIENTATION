0d420c2eece44aed78d642c7312f92c277e5d45c
Antoine Lupien <antoine.lupien.1@ens.etsmtl.ca>
2025-04-22 13:51:22 -0400

    modification de webconfig pour permettre requetes depuis le serveur

--- orienter-ets-projet/02 - backend/Sprintboot project/src/main/java/com/nextgen/backend/config/WebConfig.java
+++ orienter-ets-projet/02 - backend/Sprintboot project/src/main/java/com/nextgen/backend/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.nextgen.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://nextgenorientationets.azurewebsites.net"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
